88ec4e063b927373d0e27b54deb232b982d0f64d
Yunjae Lee <lyj7694@gmail.com>
2020-07-31 12:08:54 +0900

    Add installation script
    Signed-off-by: Yunjae Lee <lyj7694@gmail.com>

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BINDIR=/home/$USER/.local/bin
+LIBDIR=/home/$USER/.local/lib/maptrace
+BIN=$BINDIR/maptrace
+PIN=$(realpath pin/pin)
+MAPTRACE=$(realpath pin/source/tools/maptrace/obj-intel64/maptrace.so)
+
+mkdir -p $BINDIR
+mkdir -p $LIBDIR
+
+cp $MAPTRACE $LIBDIR
+
+echo "#!/bin/bash" > $BIN
+echo "CMD=\"\$@\"" >> $BIN
+echo "$PIN -t $LIBDIR/maptrace.so -- \$CMD" >> $BIN
+
+chmod +x $BIN
